29648a4bce5a6b9a51ec1280a121c71e35f04f1f
Michelle Scott <scottmm374@gmail.com>
2019-08-11 19:15:13 -0400

    dbclick, click and load events done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+
+
+
+
+// Alert when Dbclick on buttons
+const button = document.querySelectorAll('.btn')
+button.forEach((b) => {
+    b.addEventListener('dbclick', (e) => {
+        alert('Thank you for visiting')
+    })
+
+})
+
+//Alert when page loads
+
+window.addEventListener("load", (event) => {
+    alert("Welcome to the Fun Bus!");
+    
+});
+
+const navLink = document.querySelectorAll('.nav-link');
+navLink.forEach((a) => {
+    a.addEventListener('click', (e) => {
+        e.target.style.color = 'orange';
+    })
+})
