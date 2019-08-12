ef26845a36b0e0dfa339c26e11a2ad9d4cc0cbaa
Michelle Scott <scottmm374@gmail.com>
2019-08-11 20:30:32 -0400

    Stop propagation in cta section

--- js/index.js
+++ js/index.js
@@ -6,25 +6,36 @@
 // Alert when Dbclick on buttons
 const button = document.querySelectorAll('.btn')
 button.forEach((b) => {
-    b.addEventListener('dbclick', (e) => {
+    b.addEventListener('dblclick', (e) => {
         alert('Thank you for visiting')
     })
 
 })
 
-const ctaColor = document.querySelectorAll('.destination p')
-    ctaColor.forEach((text) => {
-        text.addEventListener('mouseover', (e) => {
-            e.target.style.color = 'orange';
+const ctaColor = document.querySelectorAll('.destination ')
+ctaColor.forEach((text) => {
+    text.addEventListener('mouseover', (e) => {
+        e.target.style.color = 'orange';
     })
-    
+
+})
+
+// Stop propagation (without H4 turns Orange)
+
+const headings = document.querySelectorAll('.destination h4')
+headings.forEach((text) => {
+    text.addEventListener('mouseover', (e) => {
+        e.stopPropagation();
+        e.target.style.fontSize = '3rem';
     })
+
+})
 
 //Alert when page loads
 
 window.addEventListener("load", (event) => {
     alert("Welcome to the Fun Bus!");
-    
+
 });
 
 //Nav color change on click
@@ -32,15 +43,17 @@ window.addEventListener("load", (event) => {
 const navLink = document.querySelectorAll('.nav-link');
 navLink.forEach((a) => {
     a.addEventListener('click', (e) => {
+        e.preventDefault();
         e.target.style.color = 'orange';
     })
 })
 
-//Nav prevent Default
 
-const Nav = document.querySelectorAll('.nav-link');
-navLink.addEventListener('click', (e) => {
-    e.preventDefault();
-})
+
+//Resize 
+
+window.addEventListener("resize", (event) => {
+    console.log("reportWindowSize");
+});
 
 
